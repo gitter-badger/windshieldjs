91f0e8bfe08afdc3a0972e173b1c48b04597064d
Mac Heller-Ogden <mac@machellerogden.com>
2015-07-29 11:50:15 -0500

    update

--- app/config.js
+++ app/config.js
@@ -1,17 +1,15 @@
 var path = require('path'),
     config = require('../config.json'),
-    owcs,
-    owcsPluginMis;
+    owcs;
 
 config.approot = path.join(__dirname, '..');
 
 owcs = require(path.join(config.approot, 'lib', 'owcs'));
-owcsPluginMis = require(path.join(config.approot, 'lib', 'owcs-plugin-mis'));
+owcs.configure(config.owcs);
+owcs.registerPlugin(require(path.join(config.approot, 'lib', 'owcs-plugin-mis')));
 
 module.exports = function (server) {
 
-    owcs.configure(config.owcs);
-    owcs.registerPlugin(owcsPluginMis);
 
     server.connection({ port: config.port });
 
