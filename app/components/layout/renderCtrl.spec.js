42a716b787fc5830494185357160fba40c9399de
Mac Heller-Ogden <mac@machellerogden.com>
2015-08-04 08:55:33 -0500

    adding to specs

--- app/components/layout/renderCtrl.spec.js
+++ app/components/layout/renderCtrl.spec.js
@@ -1,7 +1,28 @@
-var renderCtrl = require('./renderCtrl');
+var renderCtrl = require('./renderCtrl'),
+    Promise = require('bluebird');
 
 describe("renderCtrl", function () {
+    var mockReply,
+        mockData;
+
+    beforeEach(function () {
+        mockReply = {
+            view: function () {}
+        };
+        mockData = {
+            layout: 'OneColumnPageLayout'
+        };
+    });
+
     it("should exist", function () {
         expect(renderCtrl).toEqual(jasmine.any(Function));
+    });
+
+    it("returns a function", function () {
+        expect(renderCtrl(mockReply)).toEqual(jasmine.any(Function));
+    });
+
+    it("the returned function returns a Promise", function () {
+        expect(renderCtrl(mockReply)(mockData) instanceof Promise).toBe(true);
     });
 });
