7e6d9590f890ed1c0898cdd5f7b0fd9812f5454e
Mac Heller-Ogden <mac@machellerogden.com>
2015-08-04 08:45:19 -0500

    test harness

---
+++ app/components/layout/renderCtrl.spec.js
@@ -0,0 +1,7 @@
+var renderCtrl = require('./renderCtrl');
+
+describe("renderCtrl", function () {
+    it("should exist", function () {
+        expect(renderCtrl).toEqual(jasmine.any(Function));
+    });
+});
