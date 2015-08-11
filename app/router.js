3c4674372ef57ca5e93a02c6f8c336d58178c1ec
Mac Heller-Ogden <mac@machellerogden.com>
2015-08-11 09:51:49 -0500

    no more request promise

--- app/router.js
+++ app/router.js
@@ -1,7 +1,9 @@
 var path = require('path'),
     owcsAdapter = require('./adapters/owcs'),
     components = require('./components'),
-    logger = require('./utils/logger');
+    logger = require('./utils/logger'),
+    config = require(global.configPath),
+    owcsRest = require(path.join(config.appRoot, 'lib', 'owcs-rest'));
 
 module.exports = function (server) {
 
@@ -12,6 +14,38 @@ module.exports = function (server) {
             owcsAdapter.getDataFromWebref(req.query.lookuppage)
                 .then(components.layout.renderCtrl(reply))
                 .catch(logger.error);
+        }
+    });
+
+
+    // parsed asset data
+    server.route({
+        method: 'GET',
+        path: '/asset/{type}/{id}',
+        handler: function (req, reply) {
+            var type = req.params.type,
+                id = req.params.id;
+
+            owcsRest.promises.getAssetDao(owcsRest.functions.constructAssetRef(type, id), 4)
+                .then(function (dao) {
+                    reply(dao.get());
+                })
+                .catch(logger.error);
+        }
+    });
+
+    // raw asset data
+    server.route({
+        method: 'GET',
+        path: '/raw/{type}/{id}',
+        handler: function (req, reply) {
+            var type = req.params.type,
+                id = req.params.id;
+
+            owcsRest.promises.requestAsset(owcsRest.functions.constructAssetRef(type, id))
+                .then(reply)
+                .catch(logger.error);
+
         }
     });
 
@@ -54,57 +88,3 @@ module.exports = function (server) {
 
 
 
-
-
-    /*
-    var config = require(global.configPath),
-
-    // static server - could just as easily do this via nginx
-    server.route({
-        method: 'GET',
-        path: '/static/{param*}',
-        handler: {
-            directory: {
-                path: path.join(config.appRoot, '..', 'www-cars-com-static', 'dist')
-            }
-        }
-    });
-
-    */
-
-
-
-    // the following is just for inspecting owcs data ... not used rendering
-    /* eslint vars-on-top: 0 */
-    /*
-    var owcsRest = require(path.join(config.appRoot, 'lib', 'owcs-rest'));
-    // parsed asset data
-    server.route({
-        method: 'GET',
-        path: '/asset/{type}/{id}',
-        handler: function (req, reply) {
-            var type = req.params.type,
-                id = req.params.id;
-
-            owcsRest.promises.getAssetDao(owcsRest.functions.constructAssetRef(type, id), 4)
-                .then(function (dao) {
-                    reply(dao.get());
-                })
-                .catch(logger.error);
-        }
-    });
-    // raw asset data
-    server.route({
-        method: 'GET',
-        path: '/raw/{type}/{id}',
-        handler: function (req, reply) {
-            var type = req.params.type,
-                id = req.params.id;
-
-            owcsRest.promises.requestAsset(owcsRest.functions.constructAssetRef(type, id))
-                .then(reply)
-                .catch(logger.error);
-
-        }
-    });
-    */
