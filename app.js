9fe1b47b7c87c3a4871231416f1073423caf4f87
Mac Heller-Ogden <mac@machellerogden.com>
2015-08-13 09:16:21 -0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 require('newrelic');
+/*
+require('appdynamics').profile({
+        controllerHostName: 'paid138.saas.appdynamics.com',
+        controllerPort: 443, // If SSL, be sure to enable the next line     controllerSslEnabled: true // Optional - use if connecting to controller via SSL  
+        controllerSslEnabled: true,
+        accountName: 'Carscom', // Required for a controller running in multi-tenant mode
+        accountAccessKey: 'ajjnhjks4jnh', // Required for a controller running in multi-tenant mode
+        applicationName: 'windshield',
+        tierName: 'development',
+        nodeName: 'process' // The controller will automatically append the node name with a unique number
+});
+*/
 
 var Hapi = require('hapi'),
     server = new Hapi.Server(),
