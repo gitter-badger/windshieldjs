5210bba9b5df6d89100313b1f5bfa12c97c3a8a0
Mac Heller-Ogden <mac@machellerogden.com>
2015-08-31 15:32:39 -0500

    clearer

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,14 +2,11 @@ var fs = require('fs'),
     path = require('path'),
     routes = [];
 
-(function(dir) {
-    return fs.readdirSync(dir)
-        .filter(function (file) {
-            var stat = fs.statSync(path.join(dir, file));
-            return (stat.isFile() && (path.extname(file) === '.js'));
-        });
-})(__dirname).forEach(function (file) {
-    if (file !== 'index.js') routes = routes.concat(require(path.join(__dirname, file)));
-});
+fs.readdirSync(__dirname)
+    .filter(function (file) {
+        return (fs.statSync(path.join(__dirname, file)).isFile() && (path.extname(file) === '.js'));
+    }).forEach(function (file) {
+        if (file !== 'index.js') routes = routes.concat(require(path.join(__dirname, file)));
+    });
 
 module.exports = routes;
